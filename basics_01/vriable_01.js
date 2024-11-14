e1590d8977cfb26e3de5ce70e1b47eb57141619f
Aryan Kumar <arviaryankaushik@gmail.com>
2024-11-14 08:14:29 +0530

    Add files via upload

---
+++ basics_01/vriable_01.js
@@ -0,0 +1,10 @@
+const Account_id = 144957
+
+let Account_Email = "arviaryankaushik@gmal.com"
+
+var Account_pass = "ARyan@123"  //scope prblem in old time  
+
+Address = "Raxaul"
+
+
+console.table([Account_id, Account_Email, Account_pass, Address]);
